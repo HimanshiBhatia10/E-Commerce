d8e15c252dd51df7c4bf115de4ac25d535736269
arjun7779 <138093971+arjun7779@users.noreply.github.com>
2024-05-08 23:57:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Script for navigation bar
+const bar = document.getElementById('bar');
+const close= document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+        
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+// script for scrolling
+let ScrollContrainer = document.querySelector(".pro-container");
+let leftBtn = document.getElementById("left-button");
+let rightBtn = document.getElementById("right-button");
+
+leftBtn.addEventListener("click", ()=> {
+    ScrollContrainer.style.scrollBehavior = "smooth";
+    ScrollContrainer.scrollLeft -= 900;
+});
+
+rightBtn.addEventListener("click", ()=> {
+    ScrollContrainer.style.scrollBehavior = "smooth";
+    ScrollContrainer.scrollLeft += 900;
+});
